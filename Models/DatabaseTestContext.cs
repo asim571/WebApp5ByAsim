2413d01e73228654746c6b7a6d65f987a4336b9c
asim571 <160458924+asim571@users.noreply.github.com>
2024-05-11 20:28:24 +0545

    first commit

---
+++ Models/DatabaseTestContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApp5ByAsim.Models;
+
+public partial class DatabaseTestContext : DbContext
+{
+    public DatabaseTestContext()
+    {
+    }
+
+    public DatabaseTestContext(DbContextOptions<DatabaseTestContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.Property(e => e.Id)
+                .ValueGeneratedOnAdd()
+                .HasColumnType("numeric(18, 0)");
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApp5ByAsim.Models;
+
+public partial class Product
+{
+    public decimal Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public double? Price { get; set; }
+}
