d7092943191f9c8642eff904a8c30b6d08507c89
asim571 <160458924+asim571@users.noreply.github.com>
2024-05-17 20:02:28 +0545

    changes

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,13 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
-namespace WebApp5ByAsim.Models;
-
-public partial class Product
+namespace WebApp5ByAsim.Models
 {
-    public decimal Id { get; set; }
+    public partial class Product
+    {
+        [Required(ErrorMessage = "Id is required.")]
+        public decimal Id { get; set; }
 
-    public string? Name { get; set; }
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 100 characters.")]
+        public string? Name { get; set; }
 
-    public double? Price { get; set; }
+        [Range(0, double.MaxValue, ErrorMessage = "Price must be a positive value.")]
+        public double? Price { get; set; }
+    }
 }
--- obj/Debug/net8.0/WebApp5ByAsim.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApp5ByAsim.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp5ByAsim")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2413d01e73228654746c6b7a6d65f987a4336b9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp5ByAsim")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp5ByAsim")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
